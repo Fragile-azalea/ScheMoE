b5cd075ee484bca979a3f52dd80b9f30a52ce69e
Yu Weihong <yuweihongchn@outlook.com>
2024-10-24 14:57:43 +0800

    update examples

---
+++ schemoe/examples/megatron/README.md
@@ -0,0 +1,9 @@
+# Using ScheMoE with Megatron
+
+```
+git clone https://github.com/NVIDIA/Megatron-LM.git
+cd Megatron-LM
+git checkout core_r0.8.0
+git apply ../schemoe_megatron.diff
+./train_gpt3_schemoe.sh
+```
--- schemoe/impls/overlap.py
+++ schemoe/impls/overlap.py
@@ -1,18 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from time import time
+
+import schemoe_custom_kernel
 import torch
+from torch.distributed import get_rank
 
 from ..impls import communicate as C
-import schemoe_custom_kernel
-from time import time
-from torch.distributed import get_rank
 
 
 class Compress(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, name):
-        input = schemoe_custom_kernel.compress_operation(input, name)
+        input = schemoe_custom_kernel.compress_operation(input, name, "naive")
         return input
 
     @staticmethod
@@ -33,7 +34,7 @@ class Decompress(torch.autograd.Function):
     def backward(ctx, grad):
         # grad = torch.ones_like(grad) * torch.distributed.get_rank() + torch.arange(0.0, 0.6, 0.1, device=grad.device) + torch.arange(0.00, 0.08, 0.01, device=grad.device).unsqueeze(1) + torch.arange(0.000, 0.002, 0.001, device=grad.device).unsqueeze(1).unsqueeze(1)
         # print(grad)
-        return schemoe_custom_kernel.compress_operation(grad, ctx.name), None
+        return schemoe_custom_kernel.compress_operation(grad, ctx.name, "naive"), None
 
 
 class Comm(torch.autograd.Function):
@@ -46,16 +47,23 @@ class Comm(torch.autograd.Function):
         return schemoe_custom_kernel.comm_operation(grad)
 
 
-
-
-def a2a_ffn_overlap_forward(input, expert_fn, a2a_ffn_overlap_degree, use_2dh, group, is_compress):
+def a2a_ffn_overlap_forward(
+    input, expert_fn, a2a_ffn_overlap_degree, use_2dh, group, is_compress
+):
     split_dim = 1
-    assert a2a_ffn_overlap_degree <= C.AllToAllStatus.max_num_split, "Excepting a2a_ffn_overlap_degree (%d) <= AllToAllStatus.max_num_split (%d)." % (
-        a2a_ffn_overlap_degree, C.AllToAllStatus.max_num_split)
-    assert input.shape[split_dim] % a2a_ffn_overlap_degree == 0, "Excepting input.shape[%d] (%d) be multiple of a2a_ffn_overlap_degree (%d)." % (
-        split_dim, input.shape[split_dim], a2a_ffn_overlap_degree)
+    assert a2a_ffn_overlap_degree <= C.AllToAllStatus.max_num_split, (
+        "Excepting a2a_ffn_overlap_degree (%d) <= AllToAllStatus.max_num_split (%d)."
+        % (a2a_ffn_overlap_degree, C.AllToAllStatus.max_num_split)
+    )
+    assert (
+        input.shape[split_dim] % a2a_ffn_overlap_degree == 0
+    ), "Excepting input.shape[%d] (%d) be multiple of a2a_ffn_overlap_degree (%d)." % (
+        split_dim,
+        input.shape[split_dim],
+        a2a_ffn_overlap_degree,
+    )
     C.AllToAllStatus.init(group, a2a_ffn_overlap_degree, split_dim)
-    name = 'no'
+    name = "no"
 
     split_size = input.shape[split_dim] // a2a_ffn_overlap_degree
     input_split = list(input.split(split_size, dim=split_dim))
@@ -65,11 +73,12 @@ def a2a_ffn_overlap_forward(input, expert_fn, a2a_ffn_overlap_degree, use_2dh, g
         input_split[i] = Compress.apply(input_split[i], name)
     for i in range(a2a_ffn_overlap_degree):
         input_split[i] = Comm.apply(input_split[i])
-    
+
     for i in range(a2a_ffn_overlap_degree):
         input_split[i] = Decompress.apply(input_split[i], name)
         input_split[i] = C.post_expert_permute(
-            expert_fn(C.pre_expert_permute(input_split[i], group=group)), group=group)
+            expert_fn(C.pre_expert_permute(input_split[i], group=group)), group=group
+        )
         input_split[i] = Compress.apply(input_split[i], name)
     for i in range(a2a_ffn_overlap_degree):
         input_split[i] = Comm.apply(input_split[i])
@@ -77,4 +86,3 @@ def a2a_ffn_overlap_forward(input, expert_fn, a2a_ffn_overlap_degree, use_2dh, g
         input_split[i] = Decompress.apply(input_split[i], name)
     output = torch.cat(input_split, dim=split_dim).contiguous()
     return output
-   
